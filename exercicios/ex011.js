9bcfed6bfc52af798b70d8720458e6f9c7f42b05
neyaraujo <neycomy@gmail.com>
2025-07-22 19:27:25 -0300

    atualização

---
+++ exercicios/ex011.js
@@ -0,0 +1,39 @@
+/*
+var idade = 22
+if (idade < 16) {
+    console.log('Não vota')
+}else{
+    if (idade <= 17){
+        console.log('Voto Opcional')
+    }else {
+        console.log('Voto Obrigatório')
+    }
+}
+*/
+//////////////////////////////////////////////
+/*
+var idade = 22
+console.log(`Você tem ${idade} anos.`)
+if (idade < 16){
+    console.log('Não vota')
+}else if (idade < 18 || idade > 65){
+    console.log('Voto Opcional')
+}else {
+    console.log('Voto obrigatório')
+}
+*/
+/////////////////////////////////////////////
+
+/*
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log('Bom dia!')
+}else if (hora <=18) {
+    console.log('Boa tarde!')
+}else {
+    console.log('Boa noite!')
+}
+*/
+
