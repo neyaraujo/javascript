d01cd54935eb545070b4577d021a0de2682b283f
neyaraujo <neycomy@gmail.com>
2025-07-24 20:04:21 -0300

    conclusão aula 24

---
+++ exercicios/loops/for.js
@@ -0,0 +1,13 @@
+
+//(inicio; teste; incremento)
+var c = 1; //inicio
+while (c <= 3){ //teste
+    console.log('while ' + c) //bloco de codigo
+    c++ // incremento
+}
+
+
+for (var c=1; c <=3; c++){ // for(inicio; teste; incremento)
+    console.log('for ' + c)
+}
+console.log('FIM!')
---
+++ exercicios/loops/while-doWhile.js
@@ -0,0 +1,25 @@
+
+function testeNoInicio () {
+    var c = 1
+while (c <= 6) {
+    console.log('Passo ' + c)
+    c++
+}
+}
+
+testeNoFinal()
+
+function testeNoFinal () {
+    var c = 1
+    do {
+        console.log('Passo ' + c)
+        c++
+    } while (c <= 6)
+        
+}
+
+
+
+
+
+
